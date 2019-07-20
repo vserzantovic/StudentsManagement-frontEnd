02094c31af4c68af851c0fc49b72034e70fbfc27
vs <vserzantovic@alna.lt>
2019-07-20 12:38:42 +0300

    start page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Paper from '@material-ui/core/Paper';
 import './App.css';
+import Button from "@material-ui/core/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Paper className="paper">
+
+                    <p>STUDENTS SORTING FROM FILE</p>
+
+                    <Button
+                        variant="contained"
+                        component="label"
+                    >
+                        Upload File
+                        <input
+                            type="file"
+                            style={{display: "none"}}
+                        />
+                    </Button>
+
+
+                </Paper>
+
+            </div>
+
+
+        )
+    }
 }
 
 export default App;
